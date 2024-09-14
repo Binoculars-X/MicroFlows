55bebcf8e1b060ae18fc3aec9628bac6437b566c
Mimino74 <mimino.arah@gmail.com>
2024-09-15 07:15:39 +1000

    adding test containers

--- tests/MicroFlows.Infrastructure.Tests/UnitTest1.cs
+++ tests/MicroFlows.Infrastructure.Tests/UnitTest1.cs
@@ -1,6 +1,8 @@
+using MicroFlows.Infrastructure.Tests.Sql.Bases;
+
 namespace MicroFlows.Infrastructure.Tests;
 
-public class UnitTest1
+public class UnitTest1 : SqlTestContainersTestBase
 {
     [Fact]
     public void Test1()
