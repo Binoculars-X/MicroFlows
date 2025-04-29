196c43b43e3027b5e24dcfe70273e2d8bacd2085
Mimino74 <mimino.arah@gmail.com>
2025-04-30 06:38:39 +0700

    fixing build

--- tests/MicroFlows.Tests/UseCases/OrderProcessing/OrderProcessingTests.cs
+++ tests/MicroFlows.Tests/UseCases/OrderProcessing/OrderProcessingTests.cs
@@ -64,6 +64,8 @@ public class OrderProcessingTests
         Assert.Equal("CallAsync_StartInvoiceFlow:2", flow.ContextHistory[2].CurrentTask);
         Assert.Equal("WaitForSignalAsync_InvoiceCreatedSignal:3", flow.ContextHistory[3].CurrentTask);
 
+        await Task.Delay(1000);
+
         // on the second pass the signal should be received
         var ctx2 = await _flowProvider.ExecuteFlow(ps);
         Assert.Equal(ctx.RefId, ctx2.RefId);
@@ -99,6 +101,8 @@ public class OrderProcessingTests
         Assert.Equal("CallAsync_Init:1", flow.ContextHistory[1].CurrentTask);
         Assert.Equal("CallAsync_StartInvoiceFlow:2", flow.ContextHistory[2].CurrentTask);
         Assert.Equal("WaitForSignalAsync_InvoiceCreatedSignal:3", flow.ContextHistory[3].CurrentTask);
+
+        await Task.Delay(1000);
 
         var ctx2 = await _flowProvider.ExecuteFlow(ps);
         Assert.Equal(ctx.RefId, ctx2.RefId);
